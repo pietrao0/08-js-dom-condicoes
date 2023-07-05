cc1c3dfa4f306e6666156a53af94179dbe5a7be3
Pietra Sophia <133168161+pietrao0@users.noreply.github.com>
2023-07-05 23:45:12 +0000

    resolvir o exercicío hoda do dia

---
+++ psas-hora/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas.`;
+    if (hora >= 12 && hora <12) {
+        //Bom Dia
+        img.src = 'img/FotodaManhã.jpeg'
+        document.body.style.background = '#ffd889'
+    } else if(hora >= 12 && hora <18) {
+        //Boa Tarde
+        img.src = 'Foto/daTarde.jpeg'
+        document.body.style.background = '#fe775a'
+    } else {
+        //Boa Noite
+        img.src = 'Foto/daNoite.jpeg'
+        document.body.style.background = '#080b10'
+    }
+}
